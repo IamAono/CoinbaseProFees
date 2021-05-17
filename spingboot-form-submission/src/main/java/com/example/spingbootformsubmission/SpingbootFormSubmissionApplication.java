0066ae8c685cf6ba1d142e55f7d36d08eb5a8993
S.L <43483411+IamAono@users.noreply.github.com>
2021-05-17 14:34:29 -0700

    Initial commit

---
+++ spingboot-form-submission/src/main/java/com/example/spingbootformsubmission/SpingbootFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingbootformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootFormSubmissionApplication.class, args);
+	}
+
+}
