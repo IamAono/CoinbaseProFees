7ecd1afea3d4ebcb9b28646351f558bfe3acde86
S.L <43483411+IamAono@users.noreply.github.com>
2021-05-22 13:00:52 -0700

    reformatted FeeCalculator

--- CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/FeeCalculator.java
+++ CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/FeeCalculator.java
@@ -4,27 +4,20 @@ import java.util.ArrayList;
 
 public class FeeCalculator {
 	
-	private double profit = 0;
-	private double fees = 0;
+	double profit;
+	double fees;
 	
-	public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+	int[] tiers = {10000, 50000, 100000, 1000000, 10000000, 50000000, 100000000, 300000000, 500000000};
 	
-	public void addTransaction(Transaction t) {
-		transactions.add(t);
-		System.out.println(t.getMakerTaker()+ " " + t.getBuySell() + " " +  t.getAmount());
-	}
-	
-	public void calculate() {
-		int[] tiers = {10000, 50000, 100000, 1000000, 10000000, 50000000, 100000000, 300000000, 500000000};
-		for(int i = 0; i < transactions.size(); i++) {
-			Transaction t = transactions.get(i);
-			if(t.getBuySell() == "buy") {
-				
-			}
-			else {
-				
+	public void calculate(Transaction t, double p, double f) {
+		fees = f;
+		int tier = tiers.length;
+		for(int i = 0; i < tiers.length; i++) {
+			if(p < tiers[i]) {
+				tier = i;
 			}
 		}
+		
 	}
 	
 	public double getProfit() {
--- CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/IndexController.java
+++ CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/IndexController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class Index {
+public class IndexController {
+	
+	double profit = 0;
+	double fees = 0;
 	
 	@GetMapping("/")
 	public String indexForm(Model model) {
-		model.addAttribute("feeCalculator", new FeeCalculator());
 		model.addAttribute("transaction", new Transaction());
 		return "index";
 	}
@@ -20,6 +22,9 @@ public class Index {
 	public String indexSubmit(@ModelAttribute FeeCalculator fc, @ModelAttribute Transaction t, Model model) {
 		model.addAttribute("feeCalculator", fc);
 		model.addAttribute("transaction", t);
+		fc.calculate(t, profit, fees);
+		profit = fc.getProfit();
+		fees = fc.getFees();
 		return "profit";
 	}
 	
