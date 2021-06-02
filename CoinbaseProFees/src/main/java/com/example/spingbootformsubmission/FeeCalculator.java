7c6f2405021f79d4ca0ce50b05dc8a4fc258ab24
S.L <43483411+IamAono@users.noreply.github.com>
2021-06-01 19:55:06 -0700

    can calculate first tier

--- CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/FeeCalculator.java
+++ CoinbaseProFees/src/main/java/com/example/spingbootformsubmission/FeeCalculator.java
@@ -11,13 +11,48 @@ public class FeeCalculator {
 	
 	public void calculate(Transaction t, double p, double f) {
 		fees = f;
+		profit = p;
 		int tier = tiers.length;
 		for(int i = 0; i < tiers.length; i++) {
-			if(p < tiers[i]) {
+			if(profit < tiers[i]) {
 				tier = i;
+				break;
 			}
 		}
-		
+		if(tier == 0) {
+			if(t.getBuySell().equals("buy")) {
+				profit -= t.getAmount();
+			}
+			else {
+				profit += t.getAmount();
+			}
+			profit -= (t.getAmount() * 0.005);
+			fees += t.getAmount() * 0.005;
+		}
+		else if(tier == 1) {
+			
+		}
+		else if(tier == 2) {
+			
+		}
+		else if(tier == 3) {
+			
+		}
+		else if(tier == 4) {
+					
+		}
+		else if(tier == 5) {
+			
+		}
+		else if(tier == 6) {
+			
+		}
+		else if(tier == 7) {
+			
+		}
+		else {
+			
+		}
 	}
 	
 	public double getProfit() {
